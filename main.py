318eee2114bf69b895f2f1a4d6a96b16040d7dd4
mmiyaji155 <m.miyaji000@gmail.com>
2023-10-10 23:19:55 +0900

    first commit

---
+++ main.py
@@ -0,0 +1,74 @@
+
+from fastapi import FastAPI
+from fastapi import Request
+from fastapi.responses import HTMLResponse
+from fastapi.responses import RedirectResponse
+from discordwebhook import Discord
+
+
+app = FastAPI()
+
+
+@app.get('/', response_class=HTMLResponse)
+def get_response(request: Request):
+    try:
+        email = request.query_params.get('mail')
+        print('email= ' + email)
+    except:
+        print('no parameters')
+        with open('./templates/thanksPage.html', "r", encoding="utf-8") as file:
+            html_content = file.read()
+        print('exception0')
+        return HTMLResponse(content=html_content)
+
+    try:
+        answer = request.query_params.get('code')
+        print('code= ' + answer)
+    except:
+        print('no parameters')
+        with open('./templates/thanksPage.html', "r", encoding="utf-8") as file:
+            html_content = file.read()
+        print('exception1')
+        return HTMLResponse(content=html_content)
+
+    # answerによってreturnするtemplateを分岐
+    # 187=interests, 263=interview と定義する。
+    # 263は面談希望。面談予約サイトへリダイレクト
+    if answer == '263':
+        # with open('./templates/scheduling.html', "r", encoding="utf-8") as file:
+        #     html_content = file.read()
+        # return HTMLResponse(content=html_content)
+        code = '面談希望'
+        post_To_Discode(email, code)
+        response = RedirectResponse(url='https://meeting.eeasy.jp/kasai-lab/online')
+        return response
+    # 187は興味あり。商品紹介LPへリダイレクト
+    elif answer == '187':
+        code = '興味あり'
+        post_To_Discode(email, code)
+        response = RedirectResponse(url='https://miyajimmark.wixsite.com/my-site-1')
+        return response
+    # 例外としてthanksページを表示させる処理を書いておく。
+    else:
+        with open('./templates/thanksPage.html', "r", encoding="utf-8") as file:
+            html_content = file.read()
+        print('exception2')
+        return HTMLResponse(content=html_content)
+
+
+def post_To_Discode(email, code):
+    # メールアドレスと回答結果をdiscordに通知する
+    post = f"""
+    メールから新着の回答がありました！
+    確認の上、アクションを設定してください。
+    mail: {email}
+    回答内容: {code}
+    """
+    print(post)
+    url = "https://discord.com/api/webhooks/1152857787769045033/DnV-t3r3NlEEq9Xthz5SvOgjZ42qnwFwu8T1VQjia03zR4VBtjrUFWSjDnrVdZbfXB4Y"
+    discord = Discord(url=url)
+    discord.post(content=post)
+    print('discord posted!!')
+
+
+
